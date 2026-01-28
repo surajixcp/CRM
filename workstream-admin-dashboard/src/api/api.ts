dfc8c17bb0a1e7ef8e44e99372011234ee44fc9a
Surajixcp <surajgiri2002x@gmail.com>
2026-01-28 13:02:48 +0530

    new update

--- workstream-admin-dashboard/src/api/api.ts
+++ workstream-admin-dashboard/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://crm-1-ssit.onrender.com',
+    baseURL: 'https://crmserver.boldvibetech.com',
     headers: {
         'Content-Type': 'application/json',
     },
